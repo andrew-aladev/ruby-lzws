74be233f1208c226215689307386984516f6d7e1
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-05-22 01:55:48 +0300

    cosmetics

--- lib/lzws/version.rb
+++ lib/lzws/version.rb
@@ -1,5 +1,5 @@
 # Ruby bindings for lzws library.
-# Copyright (c) 2019 AUTHORS, distributed under the MIT License.
+# Copyright (c) 2019 AUTHORS, distributed under MIT License.
 
 module LZWS
   VERSION = "1.0.0".freeze
