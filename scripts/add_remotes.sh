0523a3b950f69c74ccc2178ee2a90b9be7578f01
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-08-08 23:59:47 +0300

    added gitlab remote

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
-git remote add github     "git@github.com:andrew-aladev/ruby-lzws.git"    || true
-git remote add bitbucket  "git@bitbucket.org:andrew-aladev/ruby-lzws.git" || true
+git remote add github    "git@github.com:andrew-aladev/ruby-lzws.git"    || true
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/ruby-lzws.git" || true
+git remote add gitlab    "git@gitlab.com:andrew-aladev/ruby-lzws.git"    || true
