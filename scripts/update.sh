a99a3c80710a599c774fddd6f229acc8e771a2b0
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-05 21:00:05 +0300

    fixed update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -l
 set -e
 
 cd "$(dirname $0)"
@@ -7,5 +7,6 @@ git fetch --all || true
 git remote | xargs -n1 -I {} git rebase "{}/master" || true
 
 cd ".."
+rvm use "."
 gem install bundler
 bundle update
