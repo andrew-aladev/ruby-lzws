dd0c23729511f1aa95787839673644c256212674
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-08 21:31:47 +0300

    updated ci test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,15 @@ set -e
 
 cd "$(dirname $0)"
 
+# Script looks overcomplicated, because it is optimized for CI machines.
+
 # Fix path environment params.
 export PATH="$PATH:/usr/local/bin"
 export C_INCLUDE_PATH="$C_INCLUDE_PATH:/usr/local/include"
 export LIBRARY_PATH="$C_INCLUDE_PATH:/usr/local/lib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
 
+# Compiling library from source.
 LZWS_BRANCH="v1.3.0"
 
 tmp="../tmp"
@@ -17,6 +20,7 @@ build="$tmp/lzws-build"
 mkdir -p "$build"
 cd "$build"
 
+# Remove orphaned directory.
 rm -rf "lzws"
 git clone --depth 1 --branch "$LZWS_BRANCH" "https://github.com/andrew-aladev/lzws.git" "lzws"
 cd "lzws/build"
@@ -24,6 +28,7 @@ cd "lzws/build"
 for dictionary in "linked-list" "sparse-array"; do
   echo "dictionary: $dictionary"
 
+  # Remove previous cmake files.
   find . \( -name "CMake*" -o -name "*.cmake" \) -exec rm -rf {} +
 
   cmake ".." \
@@ -36,10 +41,11 @@ for dictionary in "linked-list" "sparse-array"; do
     -DCMAKE_C_FLAGS_RELEASE="-O2 -march=native"
   make clean
   make -j2
+
+  # "sudo" may be required for "/usr/local".
   sudo make install
 
-  bash -c '\
-    source ~/.rvm/scripts/rvm && \
+  bash -cl '\
     cd ../../../.. && \
     gem install bundler &&
     bundle install && \
