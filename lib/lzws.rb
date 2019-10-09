5a9d6c00f216ee3bd6752e3e65f379e2a77c034a
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-09 22:00:54 +0300

    cosmetics

--- lib/lzws.rb
+++ lib/lzws.rb
@@ -1,3 +1,6 @@
+# Ruby bindings for lzws library.
+# Copyright (c) 2019 AUTHORS, MIT License.
+
 require_relative "lzws/stream/reader"
 require_relative "lzws/stream/writer"
 require_relative "lzws/file"
