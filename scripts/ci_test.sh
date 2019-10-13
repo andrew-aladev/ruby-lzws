82085d2988aaf4d4543e3f88648fa78e5f6467c7
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-14 01:14:10 +0300

    updated ci test script to work without rvm

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -6,16 +6,19 @@ cd "$(dirname $0)"
 # This script is for CI machines only, it provides junk and changes some config files.
 # Please do not use it on your machine.
 
+cd ".."
+
 # CI may not want to provide target ruby version.
 # We can just use the latest available ruby based on target major version.
-cd ".."
-ruby_version=$(< ".ruby-version")
-ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
-ruby_version=$(rvm list | grep -o -e "$ruby_major_version\.[0-9]\+" | sort | tail -n 1)
-echo "$ruby_version" > ".ruby-version"
+if [ -x "$(command -v rvm)" ]; then
+  ruby_version=$(< ".ruby-version")
+  ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
+  ruby_version=$(rvm list | grep -o -e "$ruby_major_version\.[0-9]\+" | sort | tail -n 1)
+  echo "$ruby_version" > ".ruby-version"
+fi
 
 bash -cl "\
-  rvm use '.' && \
+  (rvm use '.' || :) && \
   gem install bundler && \
   bundle install \
 "
@@ -60,7 +63,7 @@ for dictionary in "linked-list" "sparse-array"; do
 
   bash -cl "\
     cd ../../../.. && \
-    rvm use '.' && \
+    (rvm use '.' || :) && \
     bundle exec rake clean && \
     bundle exec rake \
   "
