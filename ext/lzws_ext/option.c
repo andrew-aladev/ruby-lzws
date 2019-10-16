4a56d853897e08171be56f204e48be8a64233372
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-16 22:16:22 +0300

    fixed uint options conversion

--- ext/lzws_ext/option.c
+++ ext/lzws_ext/option.c
@@ -25,6 +25,15 @@ bool lzws_ext_get_bool_option_value(VALUE options, const char* name)
   return raw_type == T_TRUE;
 }
 
+unsigned int lzws_ext_get_uint_option_value(VALUE options, const char* name)
+{
+  VALUE raw_value = get_raw_option_value(options, name);
+
+  Check_Type(raw_value, T_FIXNUM);
+
+  return NUM2UINT(raw_value);
+}
+
 unsigned long lzws_ext_get_ulong_option_value(VALUE options, const char* name)
 {
   VALUE raw_value = get_raw_option_value(options, name);
--- ext/lzws_ext/option.h
+++ ext/lzws_ext/option.h
@@ -9,20 +9,24 @@
 #include "ruby.h"
 
 bool          lzws_ext_get_bool_option_value(VALUE options, const char* name);
+unsigned int  lzws_ext_get_uint_option_value(VALUE options, const char* name);
 unsigned long lzws_ext_get_ulong_option_value(VALUE options, const char* name);
 
 #define LZWS_EXT_GET_BOOL_OPTION(options, name) \
   bool name = lzws_ext_get_bool_option_value(options, #name);
 
+#define LZWS_EXT_GET_UINT_OPTION(options, type, name) \
+  type name = lzws_ext_get_uint_option_value(options, #name);
+
 #define LZWS_EXT_GET_ULONG_OPTION(options, type, name) \
   type name = lzws_ext_get_ulong_option_value(options, #name);
 
-#define LZWS_EXT_GET_COMPRESSOR_OPTIONS(options)                         \
-  LZWS_EXT_GET_BOOL_OPTION(options, without_magic_header);               \
-  LZWS_EXT_GET_ULONG_OPTION(options, uint_fast8_t, max_code_bit_length); \
-  LZWS_EXT_GET_BOOL_OPTION(options, block_mode);                         \
-  LZWS_EXT_GET_BOOL_OPTION(options, msb);                                \
-  LZWS_EXT_GET_BOOL_OPTION(options, unaligned_bit_groups);               \
+#define LZWS_EXT_GET_COMPRESSOR_OPTIONS(options)                        \
+  LZWS_EXT_GET_BOOL_OPTION(options, without_magic_header);              \
+  LZWS_EXT_GET_UINT_OPTION(options, uint_fast8_t, max_code_bit_length); \
+  LZWS_EXT_GET_BOOL_OPTION(options, block_mode);                        \
+  LZWS_EXT_GET_BOOL_OPTION(options, msb);                               \
+  LZWS_EXT_GET_BOOL_OPTION(options, unaligned_bit_groups);              \
   LZWS_EXT_GET_BOOL_OPTION(options, quiet);
 
 #define LZWS_EXT_GET_DECOMPRESSOR_OPTIONS(options)         \
