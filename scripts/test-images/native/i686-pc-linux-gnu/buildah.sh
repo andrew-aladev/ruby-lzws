f827d1cde6fef74c1edfe18fd66d8d3b218bdf42
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-01-30 11:47:02 +0300

    fixed test images gmp static link with pic

--- scripts/test-images/native/i686-pc-linux-gnu/buildah.sh
+++ scripts/test-images/native/i686-pc-linux-gnu/buildah.sh
@@ -12,6 +12,11 @@ docker_pull "$FROM_IMAGE_NAME"
 CONTAINER=$(buildah from "$FROM_IMAGE_NAME")
 buildah config --label maintainer="$MAINTAINER" --entrypoint "/home/entrypoint.sh" "$CONTAINER"
 
+# Add pic for gmp https://bugs.gentoo.org/707332.
+run find "/usr/portage/dev-libs/gmp" -maxdepth 1 -name gmp-*.ebuild \
+  -exec sed -i "s/econf /econf --with-pic /g" "{}" \; \
+  -exec ebuild "{}" manifest \;
+
 run mkdir -p /home
 copy ../../entrypoint.sh /home/
 
