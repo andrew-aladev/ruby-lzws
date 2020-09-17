4f5ffb4262ab69bf9c15a360e2caf8cc9d643fab
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-18 00:20:25 +0300

    test image can mount current source itself without clone

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -6,9 +6,6 @@ cd "$DIR"
 
 CPU_COUNT=$(grep -c "^processor" "/proc/cpuinfo" || sysctl -n "hw.ncpu")
 
-# This script is for CI machines only, it provides junk and changes some config files.
-# Please do not use it on your machine.
-
 TMP_PATH="$(pwd)/../tmp"
 TMP_SIZE="64"
 
@@ -16,17 +13,17 @@ TMP_SIZE="64"
 
 cd ".."
 
-# CI may not want to provide target ruby version.
-# We can just use the latest available ruby based on target major version.
+ruby_version=$(< ".ruby-version")
+ruby_gemset=$(< ".ruby-gemset")
+
 if command -v rvm > /dev/null 2>&1; then
-  ruby_version=$(< ".ruby-version")
+  # Using latest available ruby version (search is based on required major version).
   ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
   ruby_version=$(rvm list | grep -o -e "${ruby_major_version}\.[0-9]\+" | sort | tail -n 1)
-  echo "${ruby_version}" > ".ruby-version"
 fi
 
 bash -cl "\
-  rvm use '.'; \
+  rvm use \"${ruby_version}@${ruby_gemset}\"; \
   gem install bundler && \
   bundle install \
 "
@@ -44,14 +41,21 @@ cd "tmp"
 
 # Remove orphaned directory.
 rm -rf "lzws"
-git clone "https://github.com/andrew-aladev/lzws.git" --single-branch --branch "$LZWS_BRANCH" --depth 1 "lzws"
+
+git clone "https://github.com/andrew-aladev/lzws.git" \
+  --single-branch \
+  --branch "$LZWS_BRANCH" \
+  --depth 1 \
+  "lzws"
 cd "lzws/build"
 
 for dictionary in "linked-list" "sparse-array"; do
   echo "dictionary: ${dictionary}"
 
-  # Remove previous cmake files.
-  find . \( -name "CMake*" -o -name "*.cmake" \) -exec rm -rf {} +
+  find . \( \
+    -name "CMake*" \
+    -o -name "*.cmake" \
+  \) -exec rm -rf {} +
 
   cmake ".." \
     -DLZWS_COMPRESSOR_DICTIONARY="$dictionary" \
@@ -60,6 +64,7 @@ for dictionary in "linked-list" "sparse-array"; do
     -DLZWS_EXAMPLES=OFF \
     -DLZWS_MAN=OFF \
     -DCMAKE_BUILD_TYPE="RELEASE"
+
   make clean
   make -j${CPU_COUNT}
 
@@ -72,7 +77,7 @@ for dictionary in "linked-list" "sparse-array"; do
 
   bash -cl "\
     cd ../../.. && \
-    rvm use '.'; \
+    rvm use \"${ruby_version}@${ruby_gemset}\"; \
     bundle exec rake clean && \
     bundle exec rake \
   "
