2667bad931b311ceff28a33471a992e38a1699de
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-12-06 13:50:31 +0300

    cosmetics

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -16,7 +16,7 @@ cd ".."
 ruby_version=$(< ".ruby-version")
 ruby_gemset=$(< ".ruby-gemset")
 
-if command -v rvm > /dev/null 2>&1; then
+if command -v "rvm" > /dev/null 2>&1; then
   # Using latest available ruby version (search is based on required major version).
   ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
   ruby_version=$(rvm list | grep -o -e "${ruby_major_version}\.[0-9]\+" | sort | tail -n 1)
@@ -50,7 +50,7 @@ git clone "https://github.com/andrew-aladev/lzws.git" \
 cd "lzws/build"
 
 # "sudo" may be required for "/usr/local".
-if command -v sudo > /dev/null 2>&1; then
+if command -v "sudo" > /dev/null 2>&1; then
   is_sudo_required=true
 else
   is_sudo_required=false
