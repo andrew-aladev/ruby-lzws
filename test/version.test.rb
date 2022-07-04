90bf636d70862b8534b0678548460c7dca63476f
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-07-04 19:10:57 +0300

    implementing adsp tests

--- test/version.test.rb
+++ test/version.test.rb
@@ -1,14 +1,15 @@
 # Ruby bindings for lzws library.
 # Copyright (c) 2019 AUTHORS, MIT License.
 
+require "adsp/test/version"
 require "lzws"
 
 require_relative "minitest"
 
 module LZWS
   module Test
-    class Version < Minitest::Test
-      def test_versions
+    class Version < ADSP::Test::Version
+      def test_version
         refute_nil LZWS::VERSION
         refute_nil LZWS::LIBRARY_VERSION
       end
